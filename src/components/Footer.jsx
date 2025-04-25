091299d7096cbd678fd9a8b9683d23696fdba59d
Abdollah <abdeajwadi@gmail.com>
2025-04-25 01:50:33 +0000

    first projet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <section className="footer">
+        <h2 className="footer-title">Votre satisfaction est notre priorité.</h2>
+        <p className="footer-text">Vous avez une question, une suggestion ou besoin d’assistance ?
+N’hésitez pas à nous contacter ! Notre équipe est là pour vous aider et vous répondre dans les plus brefs délais.
+Remplissez le formulaire ci-dessous ou envoyez-nous un email — nous serons ravis de vous écouter.
+
+</p>
+        <p>✉✈☎︎ ㊛</p>
+      </section>
+    );
+  }
+  
+  export default Footer;
